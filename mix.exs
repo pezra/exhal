069327f1864d4e40b71d130c4318fe6264ff3eac
Peter Williams <pezra@barelyenough.org>
2016-02-03 11:47:37 -0700

    bump major version due breaking api change

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExHal.Mixfile do
   def project do
     [app: :exhal,
      description: "Use HAL APIs with ease",
-     version: "1.0.0",
+     version: "2.0.0",
      elixir: "~> 1.0",
      deps: deps,
      package: package]
