a7693aba44cd36f69d3fc2fe60ac0485731cd194
Peter Williams <pezra@barelyenough.org>
2016-04-09 12:16:25 -0600

    bump version number for interpreter

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExHal.Mixfile do
   def project do
     [app: :exhal,
      description: "Use HAL APIs with ease",
-     version: "4.2.1",
+     version: "4.3.0",
      elixir: "~> 1.2",
 
      test_coverage: [tool: ExCoveralls],
