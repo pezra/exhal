86f79784bdc9d38a7e4f081685217ebd5ddeeb97
Peter Williams <pezra@barelyenough.org>
2016-04-20 22:06:50 -0600

    Convenience functions for asserting things about HAL documents

--- lib/exhal/assertions.ex
+++ lib/exhal/assertions.ex
@@ -1,51 +1,127 @@
 defmodule ExHal.Assertions do
+  @moduledoc"""
+    Convenience functions for asserting things about HAL documents
+
+    ```elixir
+    iex> import ExUnit.Assertions
+    nil
+    iex> import ExHal.Assertions
+    nil
+    iex> assert_property ~s({"name": "foo"}), "name"
+    true
+    iex> assert_property ~s({"name": "foo"}), "address"
+    ** (ExUnit.AssertionError) address is absent
+    iex> assert_property ~s({"name": "foo"}), "name", eq "foo"
+    true
+    iex> assert_property ~s({"name": "foo"}), "name", matches ~r/fo/
+    true
+    iex> assert_property ~s({"name": "foo"}), "name", eq "bar"
+    ** (ExUnit.AssertionError) expected property `name` to eq("bar")
+    iex> assert_link_target ~s({"_links": { "profile": {"href": "http://example.com" }}}),
+    ...>   "profile"
+    true
+    iex> assert_link_target ~s({"_links": { "profile": {"href": "http://example.com" }}}),
+    ...>   "item"
+    ** (ExUnit.AssertionError) link `item` is absent
+    iex> assert_link_target ~s({"_links": { "profile": {"href": "http://example.com" }}}),
+    ...>   "profile", eq "http://example.com"
+    true
+    iex> assert_link_target ~s({"_links": { "profile": {"href": "http://example.com" }}}),
+    ...>   "profile", matches ~r/example.com/
+    true
+    iex> assert_link_target ~s({"_links": { "profile": {"href": "http://example.com" }}}),
+    ...>   "profile", eq "http://bad.com"
+    ** (ExUnit.AssertionError) expected (at least one) `item` link to eq("http://bad.com") but found only http://example.com
+    iex> assert collection("{}") |> Enum.empty?
+    true
+    iex> assert 1 == collection("{}") |> Enum.count
+    ** (ExUnit.AssertionError) Assertion with == failed
+
+    """
+
   import ExUnit.Assertions
-  alias ExHal.{Document,Navigation,Link}
+  alias ExHal.{Document,Navigation,Link,Collection}
+
+  @doc"""
+    Returns a stream representation of the document.
+    """
+  def collection(doc) when is_binary(doc) do
+    Document.parse!(doc) |> collection
+  end
+  def collection(doc) do
+    Collection.to_stream(doc)
+  end
 
   @doc"""
     Returns a function that checks if the actual value is equal to the expected.
-  """
+    """
   def eq(expected) do
     fn(actual) -> actual == expected end
   end
 
   @doc"""
     Returns a function that checks if the actual value matches the expected pattern.
-  """
+    """
   def matches(expected) do
     fn(actual) -> actual =~ expected end
   end
 
   @doc"""
     Asserts that a property exists and, optionally, that its value checks out.
-  """
-  def assert_property(doc, prop_name, check_fn \\ fn (_actual) -> true end)
-
-  def assert_property(doc, prop_name, check_fn) when is_binary(doc) do
-    assert_property(Document.parse!(doc), prop_name, check_fn)
+    """
+  defmacro assert_property(doc, rel) do
+    quote do
+      p_assert_property(unquote(doc), unquote(rel), fn _ -> true end, nil)
+    end
   end
-  def assert_property(doc, prop_name, check_fn) do
-    prop_val = Document.get_property_lazy(doc, prop_name,
-      fn -> flunk "#{prop_name} is absent" end)
+  defmacro assert_property(doc, rel, check_fn) do
+    check_desc = Macro.to_string(check_fn)
 
-    assert check_fn.(prop_val)
+    quote do
+      p_assert_property(unquote(doc), unquote(rel), unquote(check_fn), unquote(check_desc))
+    end
   end
 
   @doc"""
-    Asserts that a link exists and, optionally, that its target checks out.
-  """
-  def assert_link_target(doc, rel, check_fn \\ fn (_actual) -> true end)
-
-  def assert_link_target(doc, rel, check_fn) when is_binary(doc) do
-    assert_link_target(Document.parse!(doc), rel, check_fn)
+    Asserts that the specifed link exists and that its target checks out.
+    """
+  defmacro assert_link_target(doc, rel) do
+    quote do
+      p_assert_link_target(unquote(doc), unquote(rel), fn _ -> true end, nil)
+    end
   end
-  def assert_link_target(doc, rel, check_fn) do
+  defmacro assert_link_target(doc, rel, check_fn) do
+    check_desc = Macro.to_string(check_fn)
+
+    quote do
+      p_assert_link_target(unquote(doc), unquote(rel), unquote(check_fn), unquote(check_desc))
+    end
+  end
+
+  # internal functions
+
+  def p_assert_property(doc, prop_name, check_fn, check_desc) when is_binary(doc) do
+    p_assert_property(Document.parse!(doc), prop_name, check_fn, check_desc)
+  end
+  def p_assert_property(doc, prop_name, check_fn, check_desc) do
+    prop_val = Document.get_property_lazy(doc, prop_name,
+      fn -> flunk "#{prop_name} is absent" end)
+
+    assert check_fn.(prop_val), "expected property #{prop_name} to #{check_desc}"
+  end
+
+  def p_assert_link_target(doc, rel, check_fn, check_desc) when is_binary(doc) do
+    p_assert_link_target(Document.parse!(doc), rel, check_fn, check_desc)
+  end
+  def p_assert_link_target(doc, rel, check_fn, check_desc) do
     link_targets =
       doc
       |> Document.get_links_lazy(rel, fn -> flunk "#{rel} link is absent" end)
       |> Enum.map(&Link.target_url(&1))
       |> Enum.map(&elem(&1, 1))
 
-    assert link_targets |> Enum.any?(&check_fn.(&1)), "expected `#{link_targets}` to be ..."
+    assert link_targets |> Enum.any?(&check_fn.(&1)), "expected (at least one) `#{rel}` link to #{check_desc} but found only #{link_targets}`"
   end
+
+
 end
--- test/exhal/assertions_test.exs
+++ test/exhal/assertions_test.exs
@@ -19,69 +19,127 @@ defmodule ExHal.AssertionsTest do
      } }
   end
 
-  test ".eq(expected)" do
+  test "eq(expected)" do
     assert true  == eq("foo").("foo")
     assert false == eq("foo").("bar")
   end
 
-  test ".matches(expected)" do
+  test "matches(expected)" do
     assert true  == matches(~r/f/).("foo")
     assert false == matches(~r/b/).("foo")
   end
 
-  test "property value assertions for hal doc", %{doc: doc} do
+  test "assert_property(doc, property_name)", %{doc: doc} do
     assert true == assert_property(doc, "name")
-    assert true == assert_property(doc, "name", eq "foo")
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_property(doc, nonexistent_property_name)", %{doc: doc} do
+    assert_raise ExUnit.AssertionError, ~r/absent/, fn ->
+      assert_property(doc, "nonexistent")
+    end
+  end
+
+  test "assert_property(doc, property_name, check_fn)", %{doc: doc} do
+    assert true == assert_property(doc, "name", eq "foo")
+  end
+
+  test "assert_property(doc, property_name, failing_check)", %{doc: doc} do
+    assert_raise ExUnit.AssertionError, ~r/eq."wrong"/, fn ->
       assert_property(doc, "name", eq "wrong")
     end
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_property(doc, nonexistent_prop_name, check_fn)", %{doc: doc} do
+    assert_raise ExUnit.AssertionError, ~r/absent/, fn ->
       assert_property(doc, "nonexistent", eq "foo")
     end
   end
 
-  test "property value assertions for hal str", %{hal: hal} do
+  test "assert_property(hal, property_name)", %{hal: hal} do
     assert true == assert_property(hal, "name")
-    assert true == assert_property(hal, "name", eq "foo")
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_property(hal, nonexistent_property_name)", %{hal: hal} do
+    assert_raise ExUnit.AssertionError, ~r/absent/, fn ->
+      assert_property(hal, "nonexistent")
+    end
+  end
+
+  test "assert_property(hal, property_name, check_fn)", %{hal: hal} do
+    assert true == assert_property(hal, "name", eq "foo")
+  end
+
+  test "assert_property(hal, property_name, failing_check)", %{hal: hal} do
+    assert_raise ExUnit.AssertionError, ~r/eq."wrong"/, fn ->
       assert_property(hal, "name", eq "wrong")
     end
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_property(hal, nonexistent_prop_name, check_fn)", %{hal: hal} do
+    assert_raise ExUnit.AssertionError, ~r/absent/, fn ->
       assert_property(hal, "nonexistent", eq "foo")
     end
   end
 
-  test "link target assertions for hal doc", %{doc: doc} do
+  test "assert_link_target(doc, rel)", %{doc: doc} do
     assert true == assert_link_target(doc, "profile")
+  end
+
+  test "assert_link_target(doc, nonexistent_rel)", %{doc: doc} do
+    assert_raise ExUnit.AssertionError, ~r/absent/i, fn ->
+      assert_link_target(doc, "nonexistent")
+    end
+  end
+
+  test "assert_link_target(doc, rel_with_multiple_links, check_fn)", %{doc: doc} do
     assert true == assert_link_target(doc, "profile", eq "http://example.com/simple")
     assert true == assert_link_target(doc, "profile", eq "http://example.com/other")
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_link_target(doc, rel, non_matching_target_url)", %{doc: doc} do
+    assert_raise ExUnit.AssertionError, ~r(eq.*"http://example.com/unsupported"), fn ->
       assert_link_target(doc, "profile", eq "http://example.com/unsupported")
     end
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_link_target(doc, nonexistent_rel, check_fn)", %{doc: doc} do
+    assert_raise ExUnit.AssertionError, ~r/absent/, fn ->
       assert_link_target(doc, "nonexistent", eq "http://example.com/simple")
     end
   end
 
-  test "link target assertions for hal str", %{hal: hal} do
+  test "assert_link_target(hal, rel)", %{hal: hal} do
     assert true == assert_link_target(hal, "profile")
-    assert true == assert_link_target(hal, "profile", eq "http://example.com/simple")
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_link_target(hal, nonexistent_rel)", %{hal: hal} do
+    assert_raise ExUnit.AssertionError, ~r/absent/i, fn ->
+      assert_link_target(hal, "nonexistent")
+    end
+  end
+
+  test "assert_link_target(hal, rel_with_multiple_links, check_fn)", %{hal: hal} do
+    assert true == assert_link_target(hal, "profile", eq "http://example.com/simple")
+    assert true == assert_link_target(hal, "profile", eq "http://example.com/other")
+  end
+
+  test "assert_link_target(hal, rel, non_matching_target_url)", %{hal: hal} do
+    assert_raise ExUnit.AssertionError, ~r(eq.*"http://example.com/unsupported"), fn ->
       assert_link_target(hal, "profile", eq "http://example.com/unsupported")
     end
+  end
 
-    assert_raise ExUnit.AssertionError, fn ->
+  test "assert_link_target(hal, nonexistent_rel, check_fn)", %{hal: hal} do
+    assert_raise ExUnit.AssertionError, ~r/absent/, fn ->
       assert_link_target(hal, "nonexistent", eq "http://example.com/simple")
     end
   end
 
+  test "assert collection(doc) |> Enum.empty?", %{doc: doc} do
+    assert collection(doc) |> Enum.empty?
+  end
 
-
+  test "assert collection(hal) |> Enum.empty?", %{hal: hal} do
+    assert collection(hal) |> Enum.empty?
+  end
 end
