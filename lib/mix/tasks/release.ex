d8bb9e6cf1b9f861560c944a5b5b5a4c7fc2e7d0
Peter Williams <pezra@barelyenough.org>
2016-11-03 23:07:40 -0600

    fix release task

--- lib/mix/tasks/release.ex
+++ lib/mix/tasks/release.ex
@@ -5,12 +5,12 @@ defmodule Mix.Tasks.Release do
   alias Mix.Hex.Build
 
   def run(_) do
-    meta = Build.prepare_package![:meta]
+    meta = Build.prepare_package[:meta]
 
     System.cmd "git", ["tag", "v#{meta[:version]}"]
     System.cmd "git", ["push", "--tags"]
 
     Mix.Tasks.Hex.Publish.run([])
-    Mix.Tasks.Hex.Docs.run([])
+    Mix.Tasks.Hex.Publish.run(["docs"])
   end
 end
