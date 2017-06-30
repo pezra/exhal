566fcb780a7be038c7eabbbfd23c9b5e64eac3a8
David Martinez <dave.h.martinez@gmail.com>
2017-06-30 13:33:53 -0400

    Updates to CI testing
    * Add OTP 20 to TravisCI testing
    * Exclude unsupported combo of elixir 1.3 / OTP 20
    * Test with 2.x and 3.x versions of Poison, with groundwork for
      allowing other dependency versions to be overridden via
      environment variable

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule ExHal.Mixfile do
       {:exvcr, "~> 0.7", only: :test},
       {:excoveralls, "~> 0.4", only: :test}
     ]
+    |> dep_version_overrides
   end
 
   defp package do
@@ -52,4 +53,37 @@ defmodule ExHal.Mixfile do
   defp excluded_files do
     [ "lib/mix/tasks/release.ex" ]
   end
+
+  # For testing with continuous integration systems, it is possible to force
+  # the dependency version on packages for which we allow more than one version.
+  #
+  # For example, say we want to test with two versions of the 'ExWidget' package.
+  # The deps may look like this:
+  # defp deps do
+  #   [ ..., {:ex_widget, "~> 1.5 or ~> 2.2"}, ... ]
+  # end
+  #
+  # Step 1: Add :ex_widget to the @overridable_deps list (below)
+  # Step 2: Set the environment variable EXHAL_EX_WIDGET_VERSION before compiling, e.g.
+  #   EXHAL_EX_WIDGET_VERSION=2.2.3 mix do deps.get, deps.compile
+  #   EXHAL_EX_WIDGET_VERSION="~> 1.5" mix do deps.get, deps.comile
+
+  @overridable_deps [:poison]
+  defp dep_version_overrides(deps_list) do
+    @overridable_deps
+    |> Enum.map(&({&1, env_var_value_for_dep(&1)}))
+    |> Enum.reduce(deps_list, &override_dep/2)
+  end
+
+  defp env_var_value_for_dep(dep_atom) do
+    dep_str = dep_atom |> Atom.to_string |> String.upcase
+    System.get_env("EXHAL_#{dep_str}_VERSION")
+  end
+
+  defp override_dep({_package, nil}, deps_list), do: deps_list
+  defp override_dep({package, version}, deps_list) do
+    Enum.reject(deps_list, fn(dep) -> elem(dep, 0) == package end) ++
+      [{package, version}]
+  end
+
 end
